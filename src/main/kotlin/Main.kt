c294198633d3ab65c93f5baf5bf8ea09c96bb582
alex mutisya <alexrichamerican@gmail.com>
2023-05-11 15:52:37 +0300

    WHEN STATEMENT
    WHEN KOTLIN

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+   val x=-1
+when(x){1,2,3,4,5,->println("x positive number less than 6")
+0->println("x is zero")
+-1,-2,-3,-4->println("x is a negative number greater than 5")}
+
+
+
+
+}
+
+
